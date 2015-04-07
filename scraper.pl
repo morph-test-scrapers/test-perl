e04d450d6020f2973922746deb36baa216d39038
Matthew Landauer <matthew@oaf.org.au>
2015-04-08 05:49:22 +1000

    Turn off output buffering

--- scraper.pl
+++ scraper.pl
@@ -3,6 +3,9 @@ use Database::DumpTruck;
 use strict;
 use warnings;
 
+# Turn off output buffering
+$| = 1;
+
 my $dt = Database::DumpTruck->new({dbname => 'data.sqlite', table => 'data'});
 
 $dt->insert({
